92e7d156c2e5263861470e675af7edf402aa62c2
michaelspeed <michael.the.don@gmail.com>
2019-07-02 12:05:10 +0530

    initialized

---
+++ src/Components/Hero.js
@@ -0,0 +1,52 @@
+import React from 'react';
+// State -> Immutable
+// Prop -> Properties
+// this -> Context of the class
+
+// a = 1
+// a get deleted <- value is undefined
+// a = null <- value is null
+// a = 2
+// a = 2
+
+// Immutable Variable
+// a = 1
+// {a, a} = 1,
+// a = 1 , a = 1,
+// a get deleted , a = 1
+// a = null, a = 1
+// a = 1, a = 2,
+// a is deleted, a = 2,
+// a = 2
+
+// {condition ? "true" : "false"}
+
+class Hero extends React.Component { 
+
+  state = {
+    bos: false
+  }
+
+  heroChang = () => { // <- Arrow head function
+    this.setState({
+      bos: !this.state.bos
+    })
+  }
+
+  someFunc = (id) => { 
+    // do something with id
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.bos ? <h1>Greates Hero is Hulk</h1> :  <h1>Greates Hero is Iron Man</h1>}
+        <button onClick={() => {
+          this.heroChang()
+         }}>Switch</button>
+      </div>
+    )
+  }
+}
+
+export default Hero
